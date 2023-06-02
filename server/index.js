f77c8c9a12732ead0ac96b0a90e77d54d5191b57
PavithraDhuraisamy <pavithjesus@gmail.com>
2023-06-02 22:20:28 +0530

    update code

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import routes from './routes/SalesPipeline.js'
 
 import cors from 'cors';
 
-const port = process.env.PORT || 4000
+const port = 4000
 const app = express();
 dotenv.config();
 app.use(express.json());
@@ -25,8 +25,12 @@ app.use(cors())
 
 app.use("/", routes)
 
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
     connect();
 })
+app.get('/', (req, res) => {
+    res.send('Server is connected!'); // Sending the text message as the response
+});
 
